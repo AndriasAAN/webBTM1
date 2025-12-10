21971121c477292e8545e1836599505641e35784
Andrias Setiawan <rahmawan3114@gmail.com>
2025-12-10 04:17:26 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://okes.di

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'okes.disway.id',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
