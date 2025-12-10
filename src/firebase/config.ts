63b3ac10f8d808883f0b375294961c5e107c6bdf
Andrias Setiawan <rahmawan3114@gmail.com>
2025-12-10 02:43:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9151359632-bd46f",
+  "appId": "1:356233158130:web:c24631bfc23bff38b6df6b",
+  "apiKey": "AIzaSyB232BbTKoj_ONwCmK2QvOyJl9H2cRJNRo",
+  "authDomain": "studio-9151359632-bd46f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "356233158130"
+};
