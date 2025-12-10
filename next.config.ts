c76aea0b536beecda68534e6f6074bec8fc67380
Andrias Setiawan <rahmawan3114@gmail.com>
2025-12-10 03:29:07 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://sconten

--- next.config.ts
+++ next.config.ts
@@ -23,8 +23,15 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fplm4-1.fna.fbcdn.net',
         port: '',
         pathname: '/**',
       },
